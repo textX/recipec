a70610ce099dd5cdd47bef261be16befc53047b5
Igor Dejanovic <igor.dejanovic@gmail.com>
2020-02-22 13:47:34 +0100

    Simplify and cleanup md generator

--- recipec/code_gen.py
+++ recipec/code_gen.py
@@ -1,38 +1,31 @@
-from textx import generator, get_children_of_type
-from os.path import dirname, abspath, join, splitext, basename, exists
-import click
+from functools import partial
+from textx import (generator, get_children_of_type, get_output_filename,
+                   gen_file)
+from os.path import dirname, abspath, join
 
 
 @generator('recipe-plan', 'md')
-def plan_generator(metamodel, model, output_path, overwrite, debug, **custom_args):
-    input_file = model._tx_filename
-    base_dir = output_path if output_path else dirname(input_file)
-    base_name, _ = splitext(basename(input_file))
-    output_file = abspath(
-        join(base_dir, "{}.{}".format(base_name, 'md')))
-    if overwrite or not exists(output_file):
-        click.echo('-> {}'.format(output_file))
-        model_export(model, output_file)
-    else:
-        click.echo('-- Skipping: {}'.format(output_file))
+def plan_generator(metamodel, model, output_path, overwrite, debug,
+                   **custom_args):
+    output_file = get_output_filename(model._tx_filename, output_path, 'md')
+    gen_file(model.file_name, output_file,
+             partial(model_export, model, output_file),
+             overwrite)
 
 
 def model_export(model, output_file):
     import jinja2
-    plan = get_children_of_type("Plan",model)
-    assert len(plan)==1
-    plan=plan[0]
+    plan, = get_children_of_type("Plan", model)
 
-    all_ingredients = {} # ingredientType, count
-    for e in get_children_of_type("PlanEntry",plan):
-        for i in get_children_of_type("Ingredient",e.get_recipe()):
+    all_ingredients = {}  # ingredientType -> count
+    for e in get_children_of_type("PlanEntry", plan):
+        for i in get_children_of_type("Ingredient", e.get_recipe()):
             all_ingredients[i.get_type()] = \
-                all_ingredients.get(i.get_type(),0.0) \
+                all_ingredients.get(i.get_type(), 0.0) \
                 + i.get_count_in_default_units(float(e.person_count))
-            #print("{} {}".format(i.get_type().name, all_ingredients[i.get_type()]))
 
     config = get_all(model, "Config")
-    config=config[0]
+    config = config[0]
 
     this_folder = dirname(abspath(__file__))
     jinja_env = jinja2.Environment(
@@ -46,7 +39,7 @@ def model_export(model, output_file):
 
 
 def get_all(model, what):
-    lst=[]
+    lst = []
     for m in model._tx_model_repository.all_models.filename_to_model.values():
         lst = lst + get_children_of_type(what, m)
     return lst
